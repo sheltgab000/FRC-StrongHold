f1320c5272971d039c9b8cb9017324a04768236c
Blake Johnson <crashingacastle@gmail.com>
2016-01-30 12:22:45 -0500

    Added Commits to Drive.java

--- src/org/usfirst/frc/team829/robot/Drive.java
+++ src/org/usfirst/frc/team829/robot/Drive.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.RobotDrive;
 
 public class Drive {
 
-	// Fields:
+	// Fields: Integers and variables necessary for the methods inside of the class to work.
 	int transmissionStatus;
 	final int HIGH = 0;
 	final int LOW = 1;
@@ -15,23 +15,23 @@ public class Drive {
 	
 	RobotDrive drive;
 	
-	// Constructor:
+	// Constructor: Initializes and defaults the variables to be used in the class
 	public Drive(){
-		transmissionStatus = DEFAULT_STATE;
-		shifter = new DoubleSolenoid(Ports.SHIFTER_HIGH, Ports.SHIFTER_LOW);
+		transmissionStatus = DEFAULT_STATE;	// Sets default transmissionStatus
+		shifter = new DoubleSolenoid(Ports.SHIFTER_HIGH, Ports.SHIFTER_LOW);	// Initializes the Double Solenoid for shifting
 		drive = new RobotDrive(Ports.DRIVE_BACK_LEFT, Ports.DRIVE_BACK_RIGHT, Ports.DRIVE_FRONT_LEFT, Ports.DRIVE_FRONT_RIGHT);
 	}
 	
 	
 	// Methods:
-	public void transmissionPressed(){
+	public void transmissionPressed(){	// Whenever the button is pressed swaps the current value of the transmission
 		if(transmissionStatus == HIGH)
 			transmissionStatus = LOW;
 		else
 			transmissionStatus = HIGH;
 	}
 	
-	public void update(double leftSpeed, double rightSpeed){
+	public void update(double leftSpeed, double rightSpeed){	// Changes the speed of the drive accordingly as well as pushing out or pulling in the solenoid
 		drive.tankDrive(leftSpeed, rightSpeed);
 		if(transmissionStatus == HIGH)
 			shifter.set(DoubleSolenoid.Value.kForward);
