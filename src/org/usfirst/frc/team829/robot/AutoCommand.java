7f42f63dfbed448adb6facb95f1e94ac063c410a
sheltgab000 <wrt829@wcc-345-CAD-3.warren.local>
2016-01-30 15:38:26 -0500

    Auto Beginning

---
+++ src/org/usfirst/frc/team829/robot/AutoCommand.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team829.robot;
+
+abstract public class AutoCommand {
+
+	/*
+	 * This is called using the Auto class repeatedly
+	 * It will update the motors and other moving parts
+	 */
+	abstract public void update();
+	
+	/*
+	 * This is called to see if the command is complete
+	 * It is called after the update in order to see if the command sould be deleted
+	 */
+	abstract public boolean isComplete();
+	
+}
---
+++ src/org/usfirst/frc/team829/robot/DriveCommand.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team829.robot;
+
+public class DriveCommand extends AutoCommand{
+
+	double leftSpeed, rightSpeed;	//the speeds for the motors to go
+	int targetDeltaCount;			//the target change in the encoder value
+	
+	public DriveCommand(double leftSpeed, double rightSpeed, int targetDeltaCount){
+		this.leftSpeed = leftSpeed;
+		this.rightSpeed = rightSpeed;
+		this.targetDeltaCount = targetDeltaCount;
+	}
+	
+	// TODO link drive to this class 
+	@Override
+	public void update() {
+		//drive
+		
+	}
+
+	@Override
+	public boolean isComplete() {
+		int deltaCount = 0/*get the encoder count*/;
+		
+		if(Math.abs(deltaCount) >= Math.abs(targetDeltaCount))
+			return true;
+		else 
+			return false;
+	}
+	
+
+}
