b3b0615b0ebbd90114d390c61871d4f29dccf3c9
sheltgab000 <wrt829@wcc-345-CAD-3.warren.local>
2016-01-30 09:04:12 -0500

    Initialize Repository

---
+++ src/org/usfirst/frc/team829/robot/Robot.java
@@ -0,0 +1,197 @@
+
+package org.usfirst.frc.team829.robot;
+
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.DrawMode;
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.ImageType;
+import com.ni.vision.NIVision.ShapeMode;
+
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class Robot extends IterativeRobot {
+    
+	private double shootingSpeed = .90;
+	private double slowingSpeed = .20;
+	
+	enum ShooterStatus{
+		SHOOTING,
+		SLOWING,
+		STOPPED
+	}
+    
+	ShooterStatus shooterStatus;
+	
+	Talon shooter1, shooter2;
+	
+	DigitalInput stopped, slowing;
+	
+	Joystick dual;
+	
+	//CameraServer camera;
+	
+	int session;
+	
+	VisionHelper visionHelper;
+	
+	NIVision.Range TOTE_HUE_RANGE = new NIVision.Range(100, 155);	//Default hue range for yellow tote
+	NIVision.Range TOTE_SAT_RANGE = new NIVision.Range(67, 255);	//Default saturation range for yellow tote
+	NIVision.Range TOTE_VAL_RANGE = new NIVision.Range(200, 255);	//Default value range for yellow tote
+	
+	/*double AREA_MINIMUM = 0.5; //Default Area minimum for particle as a percentage of total image area
+
+	Image frame;
+	Image binaryFrame;
+	int imaqError;
+	NIVision.ParticleFilterCriteria2 criteria[] = new NIVision.ParticleFilterCriteria2[1];
+	NIVision.ParticleFilterOptions2 filterOptions = new NIVision.ParticleFilterOptions2(0,0,1,1);*/
+    
+	   public void robotInit() {
+        shooterStatus = ShooterStatus.STOPPED;
+        
+        shooter1 = new Talon(0);
+        shooter2 = new Talon(1);
+        
+        stopped = new DigitalInput(0);
+        slowing = new DigitalInput(1);
+        
+        dual = new Joystick(2);
+        SmartDashboard.putNumber("shooting speed", shootingSpeed);
+        SmartDashboard.putNumber("slowing speed", slowingSpeed);
+        
+        /*camera = CameraServer.getInstance();
+        camera.setQuality(50);
+        camera.startAutomaticCapture();
+        */
+
+        /*visionHelper = new VisionHelper();
+        visionHelper.setHueRange(100, 155);
+        visionHelper.setSatRange(min, max);
+        */
+        // the camera name (ex "cam0") can be found through the roborio web interface
+        /*session = NIVision.IMAQdxOpenCamera("cam0",
+                NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+        NIVision.IMAQdxConfigureGrab(session);
+        
+        frame = NIVision.imaqCreateImage(ImageType.IMAGE_RGB, 0);
+		binaryFrame = NIVision.imaqCreateImage(ImageType.IMAGE_U8, 0);
+		criteria[0] = new NIVision.ParticleFilterCriteria2(NIVision.MeasurementType.MT_AREA_BY_IMAGE_AREA, AREA_MINIMUM, 100.0, 0, 0); 
+		
+		//Put default values to SmartDashboard so fields will appear
+		SmartDashboard.putNumber("Tote hue min", TOTE_HUE_RANGE.minValue);
+		SmartDashboard.putNumber("Tote hue max", TOTE_HUE_RANGE.maxValue);
+		SmartDashboard.putNumber("Tote sat min", TOTE_SAT_RANGE.minValue);
+		SmartDashboard.putNumber("Tote sat max", TOTE_SAT_RANGE.maxValue);
+		SmartDashboard.putNumber("Tote val min", TOTE_VAL_RANGE.minValue);
+		SmartDashboard.putNumber("Tote val max", TOTE_VAL_RANGE.maxValue);
+		SmartDashboard.putNumber("Area min %", AREA_MINIMUM);*/
+    }
+    
+	
+    public void autonomousInit() {
+    	
+    }
+
+    
+    public void autonomousPeriodic() {
+    	
+    }
+
+    
+    public void teleopInit(){
+    	//NIVision.IMAQdxStartAcquisition(session);
+
+        /**
+         * grab an image, draw the circle, and provide it for the camera server
+         * which will in turn send it to the dashboard.
+         */
+        
+    }
+    
+    public void teleopPeriodic() {
+        
+    	SmartDashboard.putBoolean("Stop Switch", stopped.get());
+    	SmartDashboard.putBoolean("slowing Switch", slowing.get());
+    	SmartDashboard.putBoolean("Fire Button", dual.getRawButton(2));
+    	SmartDashboard.putNumber("shooterMotor1", shooter2.get());
+    	SmartDashboard.putNumber("shooterMotor2", shooter1.get());
+    	
+    	shootingSpeed = SmartDashboard.getNumber("shooting speed");
+    	slowingSpeed = SmartDashboard.getNumber("slowing speed");
+    	
+    	switch(shooterStatus){
+    	case STOPPED :
+    		if(dual.getRawButton(2))
+    			shooterStatus = ShooterStatus.SHOOTING;
+    		break;
+    	case SHOOTING:
+    		if(slowing.get())
+    			shooterStatus = ShooterStatus.SLOWING;
+    		break;
+    		
+    	case SLOWING:
+    		if(stopped.get())
+    			shooterStatus = ShooterStatus.STOPPED;
+    		break;
+    	}
+    	
+    	
+    	switch(shooterStatus){
+    	case STOPPED :
+    		shooter1.set(0);
+    		shooter2.set(0);
+    		break;
+    	case SHOOTING:
+    		shooter1.set(shootingSpeed);
+    		shooter2.set(shootingSpeed);
+    		break;
+    		
+    	case SLOWING:
+    		shooter1.set(slowingSpeed);
+    		shooter2.set(slowingSpeed);
+    		break;
+    	}
+    	
+    	
+    	/*NIVision.Rect rect = new NIVision.Rect(10, 10, 100, 100);
+    	NIVision.IMAQdxGrab(session, frame, 1);
+        NIVision.imaqDrawShapeOnImage(frame, frame, rect,
+                DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
+        //CameraServer.getInstance().setImage(frame);
+    	
+    	
+    	//Update threshold values from SmartDashboard. For performance reasons it is recommended to remove this after calibration is finished.
+		TOTE_HUE_RANGE.minValue = (int)SmartDashboard.getNumber("Tote hue min", TOTE_HUE_RANGE.minValue);
+		TOTE_HUE_RANGE.maxValue = (int)SmartDashboard.getNumber("Tote hue max", TOTE_HUE_RANGE.maxValue);
+		TOTE_SAT_RANGE.minValue = (int)SmartDashboard.getNumber("Tote sat min", TOTE_SAT_RANGE.minValue);
+		TOTE_SAT_RANGE.maxValue = (int)SmartDashboard.getNumber("Tote sat max", TOTE_SAT_RANGE.maxValue);
+		TOTE_VAL_RANGE.minValue = (int)SmartDashboard.getNumber("Tote val min", TOTE_VAL_RANGE.minValue);
+		TOTE_VAL_RANGE.maxValue = (int)SmartDashboard.getNumber("Tote val max", TOTE_VAL_RANGE.maxValue);
+
+		//Threshold the image looking for yellow (tote color)
+		NIVision.imaqColorThreshold(binaryFrame, frame, 255, NIVision.ColorMode.HSV, TOTE_HUE_RANGE, TOTE_SAT_RANGE, TOTE_VAL_RANGE);
+
+		criteria[0].lower = (float)AREA_MINIMUM;
+		//imaqError = NIVision.imaqParticleFilter4(binaryFrame, binaryFrame, criteria, filterOptions, null);
+		//Send particle count to dashboard
+		int numParticles = NIVision.imaqCountParticles(binaryFrame, 1);
+		SmartDashboard.putNumber("Masked particles", numParticles);
+		CameraServer.getInstance().setImage(binaryFrame);*/
+    }
+    
+    
+    public void testPeriodic() {
+    
+    }
+    
+    public void disabledInit(){
+    	//NIVision.IMAQdxStopAcquisition(session);
+    }
+    
+}
---
+++ src/org/usfirst/frc/team829/robot/VisionHelper.java
@@ -0,0 +1,95 @@
+package org.usfirst.frc.team829.robot;
+
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.ImageType;
+
+import edu.wpi.first.wpilibj.CameraServer;
+
+public class VisionHelper {
+
+	private boolean uploadingToServer = false;
+	
+	private int session;
+	private int imaqError;
+	private Image frame;
+	private Image binaryFrame;
+	
+	private NIVision.Range HUE_RANGE;
+	private NIVision.Range SAT_RANGE;
+	private NIVision.Range VAL_RANGE;
+	private double PARTICLE_AREA_MIN;
+	
+	private NIVision.ParticleFilterCriteria2 criteria[] = new NIVision.ParticleFilterCriteria2[1];
+	NIVision.ParticleFilterOptions2 filterOptions = new NIVision.ParticleFilterOptions2(0,0,1,1);
+	
+	public VisionHelper(){
+		session = NIVision.IMAQdxOpenCamera("cam0",
+                NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+        NIVision.IMAQdxConfigureGrab(session);
+		frame = NIVision.imaqCreateImage(ImageType.IMAGE_RGB, 0);
+		binaryFrame = NIVision.imaqCreateImage(ImageType.IMAGE_U8, 0);
+		criteria[0] = new NIVision.ParticleFilterCriteria2(NIVision.MeasurementType.MT_AREA_BY_IMAGE_AREA, PARTICLE_AREA_MIN, 100.0, 0, 0);
+	}
+	
+	public VisionHelper(String cameraName){
+		session = NIVision.IMAQdxOpenCamera(cameraName,
+                NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+        NIVision.IMAQdxConfigureGrab(session);
+		frame = NIVision.imaqCreateImage(ImageType.IMAGE_RGB, 0);
+		binaryFrame = NIVision.imaqCreateImage(ImageType.IMAGE_U8, 0);
+		criteria[0] = new NIVision.ParticleFilterCriteria2(NIVision.MeasurementType.MT_AREA_BY_IMAGE_AREA, PARTICLE_AREA_MIN, 100.0, 0, 0);
+	}
+	
+	public boolean startAquisition(){
+		try{
+			NIVision.IMAQdxStartAcquisition(session);
+		}
+		catch(Exception e){
+			System.out.println(e);
+			return false;
+			}
+		return true;
+	}
+	
+	public Image createBinaryImage(){
+    	NIVision.IMAQdxGrab(session, frame, 1);		// grab the current frame on the camera
+    	
+    	//convert the current frame to a binary image and store it to binaryImage using the HSV thresholds
+    	NIVision.imaqColorThreshold(binaryFrame, frame, 255, NIVision.ColorMode.HSV, HUE_RANGE, SAT_RANGE, VAL_RANGE);
+    	
+    	criteria[0].lower = (float)PARTICLE_AREA_MIN;
+		imaqError = NIVision.imaqParticleFilter4(binaryFrame, binaryFrame, criteria, filterOptions, null);
+    	if(uploadingToServer)
+    		CameraServer.getInstance().setImage(binaryFrame);
+    	
+    	return binaryFrame;
+	}
+	
+	public void setUploadingToServer(boolean state){
+		uploadingToServer = state;
+	}
+	
+	public boolean stopAquisition(){
+		try{
+			NIVision.IMAQdxStopAcquisition(session);
+		}
+		catch(Exception e){
+			System.out.println(e);
+			return false;
+			}
+		return true;
+	}
+	
+	public void setHueRange(int min, int max){
+		HUE_RANGE = new NIVision.Range(min, max);
+	}
+	
+	public void setSatRange(int min, int max){
+		SAT_RANGE = new NIVision.Range(min, max);
+	}
+	
+	public void setValRange(int min, int max){
+		VAL_RANGE = new NIVision.Range(min, max);
+	}
+}
