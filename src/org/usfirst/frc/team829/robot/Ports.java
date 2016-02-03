4864bf0f177abe88af6e8fa20a5606ab242f7417
sheltgab000 <wrt829@wcc-345-CAD-3.warren.local>
2016-02-03 16:55:56 -0500

    Sets up intake but not done

--- src/org/usfirst/frc/team829/robot/Ports.java
+++ src/org/usfirst/frc/team829/robot/Ports.java
@@ -8,9 +8,13 @@ public class Ports {
 	public static final int DRIVE_FRONT_RIGHT = 4;
 	public static final int SHOOTER_1 = 6;
 	public static final int SHOOTER_2 = 7;
+	public static final int INTAKE_ROLLER = 8;
+	public static final int INTAKE_PIVOT = 9;
 	
 	public static final int STOP_SWITCH = 0;
 	public static final int SLOW_SWITCH = 1;
+	public static final int INTAKE_HOME_SWITCH = 2;
+	public static final int INTAKE_BALL_DETECTOR = 3;
 	
 	public static final int SHIFTER_HIGH = 0;
 	public static final int SHIFTER_LOW = 1;
